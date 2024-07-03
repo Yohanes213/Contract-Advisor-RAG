00f1853b9e5b372c21f7c6867fcfb89a6ac68a13
Yohanes Teshome Kebede <johnteshe13@gmail.com>
2024-07-03 07:23:55 +0300

    Introduce logger setup in logger.py

---
+++ scripts/logger.py
@@ -0,0 +1,16 @@
+import logging
+
+# Configure logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Create a file handler for logging
+file_handler = logging.FileHandler('lig.log')
+file_handler.setLevel(logging.DEBUG)
+
+# Create a formatter and add it to the handler
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+
+# Add the file handler to the logger
+logger.addHandler(file_handler)
