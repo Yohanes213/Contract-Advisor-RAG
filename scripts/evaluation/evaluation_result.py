e10a7488ab7e59f0a72798b2d22b6b12e386b9c9
Yohanes Teshome Kebede <johnteshe13@gmail.com>
2024-07-05 22:44:51 +0300

    Moved evaluation_result.py to evaluation folder

---
+++ scripts/evaluation/evaluation_result.py
@@ -0,0 +1,39 @@
+import plotly.graph_objects as go
+
+def visualize_result(result, name, title):
+
+    data = {
+        'context_precision': result['context_precision'],
+        'context_recall': result['context_recall'],
+        'faithfulness': result['faithfulness'],
+        'answer_relevancy': result['answer_relevancy'],
+        'answer_similarity': result['answer_similarity']
+        
+    }
+
+
+    # Create the radar chart
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatterpolar(
+        r=list(data.values()),
+        theta=list(data.keys()),
+        fill='toself',
+        name='Ensemble Rag'
+    ))
+
+    fig.update_layout(
+        polar=dict(
+            radialaxis=dict(
+                visible=True,
+                range=[0, 1]
+            )
+        ),
+        showlegend=True,
+        title=title,
+        width=800
+    )
+
+    # Save the figure as an HTML file
+    fig.write_html(name)
+
