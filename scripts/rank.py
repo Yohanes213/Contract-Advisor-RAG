97e65326ece4720566ac7fa0b5caa940a32a5853
Yohanes Teshome Kebede <johnteshe13@gmail.com>
2024-07-05 16:35:18 +0300

    Removed the retriever process from here and imported the function for that

--- scripts/rank.py
+++ scripts/rank.py
@@ -3,13 +3,22 @@ import os
 from langchain_cohere import CohereRerank
 from langchain_community.llms import Cohere
 from langchain.retrievers.contextual_compression import ContextualCompressionRetriever
-from query_expansion import expand_query_hypothetical, expand_query_multiple
-from response_generation import retrive, generate_response
+from scripts.query_expansion import expand_query_hypothetical, expand_query_multiple
+from scripts.response_generation import generate_response
+from pinecone import Pinecone as PineconeClient
+from langchain_pinecone import PineconeVectorStore
+from langchain.embeddings.openai import OpenAIEmbeddings
 import asyncio
+from retriever import retriever
 
 load_dotenv()
 cohere_api_key = os.getenv("COHERE_API_KEYS")
 os.environ["COHERE_API_KEY"] = cohere_api_key
+openai_key = os.getenv("OPENAI_API_KEYS")
+pinecone_key = os.getenv("PINECONE_API_KEYS")
+
+
+retriever = retriever()
 
 def coherererank():
     """
@@ -20,9 +29,8 @@ def coherererank():
         ContextualCompressionRetriever: The configured retriever instance.
     """
 
-    llm = Cohere(temperature=0)  # Use Cohere for reranking
-    compressor = CohereRerank()  # Use Cohere for compression
-    retriever = retrive()  # Replace with your preferred retrieval function
+    llm = Cohere(temperature=0) 
+    compressor = CohereRerank()  
 
     compressed_retriever = ContextualCompressionRetriever(
         base_compressor=compressor, base_retriever=retriever
@@ -37,23 +45,20 @@ if __name__ == "__main__":
     joint_query = expand_query_multiple(original_query)
 
     list_queries = joint_query.strip().split('\n')
-    list_queries.append(original_query)  # Include the original query
+    list_queries.append(original_query) 
 
-    # Retrieve documents for all expanded and original queries
+    
     all_docs = []
     for query1 in list_queries:
         docs = compressed_retriever.invoke(query1)
         all_docs.extend(docs)
 
-    #print(all_docs)
-    # Rank all retrieved documents using a suitable ranking algorithm (replace with your choice)
     ranked_docs = sorted(all_docs, key=lambda doc: vars(doc)['metadata']['relevance_score'], reverse=True)  # Example ranking by score
 
     query = ''
-    # Print the top 3 ranked documents
     for doc in ranked_docs[:3]:
         print(doc)
-        print("\n")  # Add newline for better formatting
+        print("\n")  
 
         query+=vars(doc)['page_content']
         query+= "\n"
