3a6287074a385beccc7ed6b2d22eef8edf9ca6bb
Yohanes Teshome Kebede <99422479+Yohanes213@users.noreply.github.com>
2024-07-06 17:17:55 +0000

    Add a better documentation

--- scripts/query_expansion.py
+++ scripts/query_expansion.py
@@ -10,30 +10,49 @@ client = OpenAI(
 )
 
 def expand_query_hypothetical(query, model="gpt-3.5-turbo"):
+    """
+    Expands a given query with a hypothetical example answer using OpenAI's Chat API.
 
+    Parameters:
+    query (str): The original query to expand.
+    model (str): The model to use for generating the response. Default is "gpt-3.5-turbo".
+
+    Returns:
+    str: A hypothetical example answer related to the query.
+    """
     messages = [
         {
             "role": "system",
-            "content":(
+            "content": (
                 "You are a knowledgeable contract assistant. For the given question, provide a hypothetical example answer "
                 "that one might find in an Advisory Services Agreement document. Ensure the example is clear, concise, and relevant to the query."
                 "Answer in one sentence and make it like you have an information."
             )
-        
+
         },
         {"role": "user", "content": query}
-    ] 
+    ]
 
     response = client.chat.completions.create(
-        model = model,
-        messages = messages,
-        temperature = 0
+        model=model,
+        messages=messages,
+        temperature=0
     )
 
     return response.choices[0].message.content
 
 
 def expand_query_multiple(query, model="gpt-3.5-turbo"):
+    """
+    Expands a given query with multiple related questions using OpenAI's Chat API.
+
+    Parameters:
+    query (str): The original query to expand.
+    model (str): The model to use for generating the response. Default is "gpt-3.5-turbo".
+
+    Returns:
+    str: Multiple related questions to help explore different aspects of the original query.
+    """
     messages = [
         {
             "role": "system",
@@ -45,7 +64,7 @@ def expand_query_multiple(query, model="gpt-3.5-turbo"):
             )
         },
         {"role": "user", "content": query}
-    ] 
+    ]
 
     response = client.chat.completions.create(
         model=model,
@@ -53,7 +72,8 @@ def expand_query_multiple(query, model="gpt-3.5-turbo"):
         temperature=0.2
     )
 
-    return response.choices[0].message.content#.strip()
+    return response.choices[0].message.content
+
 
 if __name__ == "__main__":
     query = "Who are the parties to the Agreement and what are their defined names?"
@@ -65,4 +85,3 @@ if __name__ == "__main__":
     print(list_queries)
     for query2 in list_queries:
         print(query2)
-
